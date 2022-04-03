438303e134e46b61e0524fc4969be520de5d705e
Vinícius <euphonion@tutanota.com>
2022-04-03 19:19:34 -0300

    changed header font, add css variable

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,9 +3,7 @@ import styles from './Header.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>
-        COMIC<span className={styles.accent}>SHOP</span>
-      </h1>
+      <h1 className={styles.title}>COMICSHOP</h1>
       <form>
         <input type="text" />
         <button type="button">search</button>
