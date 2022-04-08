898c5bb11522d74c4551c10bfb69f1871ddeb595
Vinícius <euphonion@tutanota.com>
2022-04-08 18:02:12 -0300

    added link on ComicCard

--- src/components/ComicCard/ComicCard.tsx
+++ src/components/ComicCard/ComicCard.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 interface ComicCardProps {
   id: number;
@@ -16,39 +17,41 @@ export default function ComicCard({
   rare,
 }: ComicCardProps) {
   return (
-    <Flex flexDir="column" alignItems="center">
-      <Box backgroundColor="gray.400" position="relative">
-        {rare && (
-          <Box
-            zIndex="1"
-            position="absolute"
-            top="0"
-            right="0"
-            backgroundColor="red"
-            color="white"
-            padding="1"
-          >
-            <Text fontSize="3xl">Rare!</Text>
-          </Box>
-        )}
-        <Image
-          border={rare ? 'solid 5px red' : ''}
-          maxHeight="400px"
-          src={imageUrl}
-          alt={`${id}'s cover`}
-        />
-      </Box>
-      <Flex flexDir="column" alignItems="center" mt="2">
-        <Heading fontSize="xl" textAlign="center">
-          {title}
-        </Heading>
-        <Text fontSize="2xl">
-          {price.toLocaleString('en-US', {
-            style: 'currency',
-            currency: 'USD',
-          })}
-        </Text>
+    <Link to={`/comic/${id}`}>
+      <Flex flexDir="column" alignItems="center">
+        <Box backgroundColor="gray.400" position="relative">
+          {rare && (
+            <Box
+              zIndex="1"
+              position="absolute"
+              top="0"
+              right="0"
+              backgroundColor="red"
+              color="white"
+              padding="1"
+            >
+              <Text fontSize="3xl">Rare!</Text>
+            </Box>
+          )}
+          <Image
+            border={rare ? 'solid 5px red' : ''}
+            maxHeight="400px"
+            src={imageUrl}
+            alt={`${id}'s cover`}
+          />
+        </Box>
+        <Flex flexDir="column" alignItems="center" mt="2">
+          <Heading fontSize="xl" textAlign="center">
+            {title}
+          </Heading>
+          <Text fontSize="2xl">
+            {price.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            })}
+          </Text>
+        </Flex>
       </Flex>
-    </Flex>
+    </Link>
   );
 }
