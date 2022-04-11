681ec2eca91e46045263d59f45060787f1ebbc43
Vinícius <euphonion@tutanota.com>
2022-04-11 14:43:34 -0300

    layout change, added button to page

--- src/components/ComicDisplay/ComicDisplay.tsx
+++ src/components/ComicDisplay/ComicDisplay.tsx
@@ -1,5 +1,7 @@
-import { Flex, Heading, Image } from '@chakra-ui/react';
+import { Flex, Heading, Image, Divider } from '@chakra-ui/react';
+import { AiOutlineShopping } from 'react-icons/ai';
 import ComicModel from '../../models/Comic';
+import Button from '../Button';
 
 interface ComicProps {
   comic: ComicModel;
@@ -8,15 +10,32 @@ interface ComicProps {
 export default function ComicDisplay({ comic }: ComicProps) {
   return (
     <Flex height="100%" justifyContent="center" alignItems="center">
-      <Image src={comic.imageUrl} height="70%" />
-      <Flex height="50%" flexDir="column" alignItems="flex-end">
-        <Heading>{comic.title}</Heading>
-        <Heading fontSize="3xl">
-          {comic.price.toLocaleString('en-US', {
-            style: 'currency',
-            currency: 'USD',
-          })}
-        </Heading>
+      <Flex
+        shadow="dark-lg"
+        height="90%"
+        width="80%"
+        alignItems="center"
+        padding="24"
+      >
+        <Image src={comic.imageUrl} height="100%" />
+        <Divider orientation="vertical" ml="auto" />
+        <Flex
+          height="50%"
+          flexDir="column"
+          alignItems="flex-end"
+          ml="auto"
+          gap="20%"
+        >
+          <Heading>{comic.title}</Heading>
+          <Heading fontSize="3xl">
+            {comic.price.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            })}
+          </Heading>
+
+          <Button text="Add to Bag" icon={<AiOutlineShopping size="30" />} />
+        </Flex>
       </Flex>
     </Flex>
   );
