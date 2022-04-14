84ec1a729ce178f27977b3d3bae4c8b1dac6d80c
Vinícius <euphonion@tutanota.com>
2022-04-13 23:26:33 -0300

    updated comic display component

--- src/components/ComicDisplay/ComicDisplay.tsx
+++ src/components/ComicDisplay/ComicDisplay.tsx
@@ -1,5 +1,6 @@
-import { Divider, Flex, Heading, Image } from '@chakra-ui/react';
+import { Box, Divider, Flex, Heading, Image } from '@chakra-ui/react';
 import { AiOutlineShopping } from 'react-icons/ai';
+import { FaStar } from 'react-icons/fa';
 import ComicModel from '../../models/Comic';
 import Button from '../Button';
 
@@ -18,28 +19,52 @@ export default function ComicDisplay({ comic, onAdd }: ComicProps) {
         alignItems="center"
         padding="24"
       >
-        <Image src={comic.imageUrl} height="100%" />
-        <Divider orientation="vertical" ml="auto" />
         <Flex
-          height="50%"
-          flexDir="column"
-          alignItems="flex-end"
-          ml="auto"
-          gap="20%"
+          flexShrink={0}
+          flexGrow={0}
+          height="100%"
+          position="relative"
+          justifyContent="center"
+          alignItems="center"
         >
-          <Heading>{comic.title}</Heading>
-          <Heading fontSize="3xl">
+          {comic.rare && (
+            <Box
+              zIndex="1"
+              position="absolute"
+              top="0"
+              right="11%"
+              backgroundColor="red"
+              color="white"
+              padding="4"
+            >
+              <FaStar size="30" />
+            </Box>
+          )}
+          <Image
+            height="100%"
+            src={comic.imageUrl}
+            border={comic.rare ? 'solid 5px red' : ''}
+          />
+        </Flex>
+        <Divider orientation="vertical" ml="auto" />
+        <Flex height="80%" flexDir="column" alignItems="flex-end" ml="auto">
+          <Heading textAlign="right" mb="10">
+            {comic.title}
+          </Heading>
+          <Heading fontSize="5xl">
             {comic.price.toLocaleString('en-US', {
               style: 'currency',
               currency: 'USD',
             })}
           </Heading>
 
-          <Button
-            text="Add to Bag"
-            icon={<AiOutlineShopping size="30" />}
-            onClick={onAdd}
-          />
+          <Box mt="auto">
+            <Button
+              text="Add to Bag"
+              icon={<AiOutlineShopping size="30" />}
+              onClick={onAdd}
+            />
+          </Box>
         </Flex>
       </Flex>
     </Flex>
